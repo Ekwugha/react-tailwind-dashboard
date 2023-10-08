3842249457232bf286519b5351ef3f9c7b023f4e
ekwugha <elloekwugha@gmail.com>
2023-10-08 07:50:37 +0100

    buyer profile, recent order, popular product

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import DashboardStatsGrid from './DashboardStatsGrid'
-import TransactionChart from './TransactionChart'
+import React from "react";
+import DashboardStatsGrid from "./DashboardStatsGrid";
+import TransactionChart from "./TransactionChart";
+import BuyerProfileChart from "./BuyerProfileChart";
+import RecentOrders from "./RecentOrders";
+import PopularProducts from "./PopularProducts";
 
 const Dashboard = () => {
   return (
-    <div className='flex flex-col gap-4'>
+    <div className="flex flex-col gap-4">
       <DashboardStatsGrid />
-      <TransactionChart />
+      <div className="flex flex-row gap-4 w-full">
+        <TransactionChart />
+        <BuyerProfileChart />
+      </div>
+      <div className="flex flex-row gap-4 w-full">
+        <RecentOrders />
+        <PopularProducts />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -7,9 +7,9 @@ const Layout = () => {
   return (
     <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
       <Sidebar />
-      <div className="flex-1">
+      <div className="flex flex-col flex-1">
         <Header />
-        <div className="p-4">{<Outlet />}</div>
+        <div className="flex-1 p-4 min-h-0 overflow-auto">{<Outlet />}</div>
       </div>
     </div>
   );
